892ba1a97b6b97e6a3d91b78d4017fc13e4cea87
KamaMichaelis <achantayd@gmail.com>
2020-05-22 12:47:51 -0400

    arrays and callbacks complete

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -69,7 +69,7 @@ populationTotal=zooAnimals.reduce(function(acc,item){
   return acc+item.population
 },0)
 
-console.log(populationTotal);
+//console.log(populationTotal);
 
 
 // ==== Callbacks ====  
@@ -81,18 +81,34 @@ console.log(populationTotal);
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
 
-
+  function consume(a,b,cb){
+    return cb(a,b);
+  }
+  
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+const add=function(a,b){
+  return a+b
+}
 
+const subtract=function(a,b){
+  return a-b
+}
+const multiply=function(a,b){
+  return a*b
+}
+
+const greeting=function(a,b){
+  return `Hello ${a} ${b}, nice to meet you!`
+}
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 
